a9a6b850adefd2347c7a77f0577dcecd26c25381
GabrielHaddad <gabrielphaddad@yahoo.com.br>
2019-11-16 15:47:38 -0300

    removed some commentary

--- main.c
+++ main.c
@@ -23,12 +23,10 @@ int main()
             {
                 if(atoi(c) == 0)
                 {
-                    //printf(" %d ", c[0]);
                     novaTabela[i][j] = c[0];
                 }
                 else
                 {
-                    //printf(" %d ", atoi(c));
                     novaTabela[i][j] = atoi(c);
                 }
             }
@@ -56,99 +54,10 @@ int main()
 
     fillBuffer(buffer, arq);
 
-    /*
-    printf("%c", (char)getProxChar(buffer));
-    printf("%c", (char)getProxChar(buffer));
-    printf("%c", (char)getProxChar(buffer));
-
-    struct Token *token = getProxToken(buffer);
-
-    int k = 0;
-    for(k = 0; k < token->size; k++){
-        printf("%c", (char)token->content[k]);
-    }
-    */
-
-    /*
-    i = 0, j = 0, row = 10, col = 5;
-    int **tabela = (int **)malloc(sizeof(int *) * row);
-    for(i = 0; i < row; i++)
-    {
-        tabela[i] = (int *) malloc(sizeof(int) * col);
-        for(j = 0; j < col; j++)
-        {
-            tabela[i][j] = 0;
-        }
-    }
 
 
-    tabela[0][0] = 'N';
-    tabela[1][0] = 'N';
-    tabela[2][0] = 'S';
-    tabela[3][0] = 'S';
-    tabela[4][0] = 'S';
-    tabela[5][0] = 'S';
-    tabela[6][0] = 'N';
-    tabela[7][0] = 'S';
-    tabela[8][0] = 'S';
-    tabela[9][0] = 'F';
 
-    tabela[0][1] = 1;
-    tabela[1][1] = -1;
-    tabela[2][1] = -1;
-    tabela[3][1] = -1;
-    tabela[4][1] = -1;
-    tabela[5][1] = -1;
-    tabela[6][1] = -1;
-    tabela[7][1] = -1;
-    tabela[8][1] = -1;
-    tabela[9][1] = '<';
-
-    tabela[0][2] = 5;
-    tabela[1][2] = 2;
-    tabela[2][2] = -1;
-    tabela[3][2] = -1;
-    tabela[4][2] = -1;
-    tabela[5][2] = -1;
-    tabela[6][2] = 7;
-    tabela[7][2] = -1;
-    tabela[8][2] = -1;
-    tabela[9][2] = '=';
-
-    tabela[0][3] = 6;
-    tabela[1][3] = 3;
-    tabela[2][3] = -1;
-    tabela[3][3] = -1;
-    tabela[4][3] = -1;
-    tabela[5][3] = -1;
-    tabela[6][3] = -1;
-    tabela[7][3] = -1;
-    tabela[8][3] = -1;
-    tabela[9][3] = '>';
-
-    tabela[0][4] = -1;
-    tabela[1][4] = 4;
-    tabela[2][4] = -1;
-    tabela[3][4] = -1;
-    tabela[4][4] = -1;
-    tabela[5][4] = -1;
-    tabela[6][4] = 8;
-    tabela[7][4] = -1;
-    tabela[8][4] = -1;
-    tabela[9][4] = 'O';
-
-    for(i = 0; i< row; i++)
-    {
-        for(j = 0; j < col; j++)
-        {
-            printf("[%d][%d]: %d ", i,j,tabela[i][j]);
-        }
-        printf("\n");
-    }
-    */
     struct hash* ha = criaHash();
-
-    // printf("\n %d \n ",tabelaTransicao(ha, arq, buffer, tabela, row, col, 0));
 
     printf("\n\n");
     struct Token *token = tabelaTransicao(ha, arq, buffer, novaTabela, row, col, 0);
